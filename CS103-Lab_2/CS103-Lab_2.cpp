64d57d6dfa3cc92a23b44ce91a1e180b19c86be4
unknown <bermido.charles@gmail.com>
2021-10-21 11:16:04 +1300

    first commit

---
+++ CS103-Lab_2/CS103-Lab_2.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+
+struct worker {
+
+    std::string fName;
+    std::string mName;
+    std::string lName;
+    std::string streetAddress;
+    std::string 
+
+
+
+
+}
+
+int main()
+{
+
+    std::cout << "Hello World!\n";
+
+}
